b1c5a301436491f279120c5b4e8f461d56fe822e
shamshadpattani <mail4shamshadpattani@gmail.com>
2021-04-19 13:51:44 +0530

    admin push successully

---
+++ asset/js/admin.js
@@ -0,0 +1,41 @@
+new Vue({
+    components: {
+        'p-datatable' : datatable,
+        'p-row':row,
+        'p-column':column,
+        'p-columngroup':columngroup,
+        'p-inputtext':inputtext,
+    },
+    data(){
+        return {
+            editingCellRows: {},
+        editingRows: [],
+            columns: null,
+            maturityList:null
+        }
+    },
+    created(){
+        this.maturityList={
+            "data": [{
+                    "date": "7 days to 14 days",
+                    "n": "2.50%",
+                    "s": "3.00%"
+                },
+                {
+                    "date": "15 days to 29 days",
+                    "n": "2.50%",
+                    "s": "3.00%"
+                },
+                {
+                    "date": "30 days to 45 days",
+                    "n": "3.00%",
+                    "s": "3.50%"
+                },
+                {
+                    "date": "46 days to 60 days",
+                    "n": "3.00%",
+                    "s": "3.50%"
+                }
+            ]
+        }
+    }}).$mount('#app')
